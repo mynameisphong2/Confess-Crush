21fa6410b867195ad10916d8f797ea4c1c24860d
mynameisphong2 <96510512+mynameisphong2@users.noreply.github.com>
2021-12-22 11:30:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    titleWeb: "mylove",
-    introTitle: 'kmy ơi',
-    introDesc: `Anh iu em`,
+    titleWeb: "kmysiucute",
+    introTitle: 'gửi công chúa của anh :3',
+    introDesc: `Nguyễn Nguyễn Kiều My`,
     btnIntro: 'hihi :3',
     title: 'Đừng dận anh nữa nha công chúa <3',
     desc: 'Anh iu em lắm:3',
@@ -9,9 +9,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới này có hơn 7 tỉ người mà sao em lại chọn anh <3',
     btnReply: 'Gửi cho tphongg <3',
-    reply: 'Vì em iu anh😽',
+    reply: 'Vì em iu anhh <3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Mãi iu công túa của anh',
+    messDesc: 'Mãi iu công chúa của anhh',
     btnAccept: 'iu anhhhh <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
